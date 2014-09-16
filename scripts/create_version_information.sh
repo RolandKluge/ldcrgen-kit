44c86d508bcecff5640c4834f7b14f55615e8db0
Roland Kluge <roland.kluge@gmx.de>
2014-09-16 22:12:29 +0200

    Clean up build.xml
     - remove superfluous version information

--- scripts/create_version_information.sh
+++
@@ -1,7 +0,0 @@
-﻿#!/bin/bash
-
-versionFile=“../version.txt“
-rm -f $versionFile
-echo "SVN Revision:" >> $versionFile
-next_revision=$(echo $(svnversion) + 1 | sed -e 's/M//' | bc)
-echo $next_revision >> $versionFile
