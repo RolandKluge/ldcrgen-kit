2083a7ef7ee77d81684d0cff1524ba20a37e53f2
atlantisZone <amin.rigi@gmail.com>
2016-11-07 18:04:27 +1100

    Added ConvertGraphjToText function
    ConvertGraphjToText function is added. This function stores the initial network and its nodes into two separate text files. It also stores the atomic change stream in another text file.

--- src/edu/kit/iti/ldcrgen/converter/BinaryToTextConverter.java
+++ src/edu/kit/iti/ldcrgen/converter/BinaryToTextConverter.java
@@ -82,4 +82,112 @@ public class BinaryToTextConverter
 			clusterOpCounter++;
 		}
 	}
+	
+	 /**
+	 * Converts the given binary file (first argument) into a  text file.
+	 *  graphj_path: the binary file 
+	 *  nodesPath: A text file which contains names of the nodes
+	 *  netPath: A text file which contains the network
+	 *  changeLog: A text file which contains the stream of atomic changes
+	 */
+	public static void ConvertGraphjToText(string graphj_path, string nodesPath, string netPath, string changeLog)
+	{
+		
+		
+		BufferedInputStream fstream = null;
+		try
+		{
+			fstream = new BufferedInputStream(new FileInputStream(graphj_path));
+		}
+		catch (final FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+
+		final GraphJournal gJournal = GraphJReader.readGraphJournal(fstream);
+		final ClusteringJournal clJournal = GraphJReader.readClusteringJournal(fstream);
+
+		if (null != fstream)
+		{
+			try
+			{
+				fstream.close();
+			}
+			catch (final IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+
+
+		int graphOpCounter = 1;
+		int node_id = 1;
+		boolean sw = true;
+		try
+		{
+			File file_nodes = new File(nodesPath);
+			FileWriter fw_nodes = new FileWriter(file_nodes.getAbsoluteFile());
+			BufferedWriter bw_nodes = new BufferedWriter(fw_nodes);
+			
+			File file_net = new File(netPath);
+			FileWriter fw_net = new FileWriter(file_net.getAbsoluteFile());
+			BufferedWriter bw_net = new BufferedWriter(fw_net);
+			
+			File file_change = new File(changeLog);
+			FileWriter fw_change = new FileWriter(file_change.getAbsoluteFile());
+			BufferedWriter bw_change = new BufferedWriter(fw_change);
+			
+			
+			for (final GraphOperation op : gJournal)
+			{
+				
+				switch(op.type)
+				{
+					case CreateNode:
+						if(sw){
+							bw_nodes.write(node_id + " " + op.arg0 + "\n");
+							node_id++;
+						}
+						else{
+							bw_change.write("ADD NODE" + "\n" +node_id + " " + op.arg0 + "\n");
+							node_id++;
+						}
+							
+						break;
+						
+						
+					case CreateEdge:
+						if(sw) bw_net.write(op.arg0 + " " + op.arg1 + "\n");
+						else bw_change.write("ADD EDGE" + "\n" + op.arg0 + " " + op.arg1 + "\n"); 
+						break;
+						
+					case NextStep:
+						sw = false;
+						bw_change.write("NEXT STEP"+"\n"); 
+						break;
+					
+
+					
+					case RemoveEdge:
+						bw_change.write("REMOVE EDGE" + "\n" + op.arg0 + " " + op.arg1 + "\n");
+						break;	
+						
+					case RemoveNode:
+						bw_change.write("REMOVE NODE" + "\n" + op.arg0 + " " + op.arg1 + "\n");
+						break;
+						
+				}
+				
+				graphOpCounter++;
+			}
+			
+			bw_change.close();
+			bw_net.close();
+			bw_nodes.close();
+		}
+		catch (final IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
